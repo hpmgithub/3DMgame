6136e111a1cf8b03655b1b060f413f0dec44e6f2
cnso <hdmj@sina.com>
2015-11-20 12:01:37 +0800

    添加三个主的Fragment

--- app/src/main/java/com/jash/dmgame/MainActivity.java
+++ app/src/main/java/com/jash/dmgame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jash.dmgame;
 
+import android.app.Activity;
 import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
@@ -7,6 +8,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,12 +24,18 @@ import android.view.MenuItem;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.jash.dmgame.databinding.NavHeaderBinding;
+import com.jash.dmgame.fragments.ChapterFragment;
+import com.jash.dmgame.fragments.ForumFragment;
+import com.jash.dmgame.fragments.GameFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private DrawerLayout drawer;
+    private ChapterFragment chapter;
+    private ForumFragment forum;
+    private GameFragment game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,17 @@ public class MainActivity extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, 0, 0);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        chapter = new ChapterFragment();
+        forum = new ForumFragment();
+        game = new GameFragment();
+        transaction.add(R.id.main_container, chapter)
+                .add(R.id.main_container, forum)
+                .add(R.id.main_container, game)
+                .detach(forum)
+                .detach(game)
+                .detach(chapter)
+                .attach(chapter).commit();
     }
 
     @Override
@@ -53,7 +73,24 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        switch (item.getItemId()){
+            case R.id.nav_chapter:
+                transaction.detach(forum).detach(game).attach(chapter);
+                break;
+            case R.id.nav_forum:
+                transaction.detach(chapter).detach(game).attach(forum);
+                break;
+            case R.id.nav_game:
+                transaction.detach(forum).detach(chapter).attach(game);
+                break;
+            case R.id.nav_setting:
+                break;
+            case R.id.nav_exit:
+                ActivityCompat.finishAffinity(this);
+                break;
+        }
+        transaction.commit();
 //
         drawer.closeDrawer(GravityCompat.START);
         return true;
